d7783f197b50b9ecbd1c57d69d345cb7bc2731f4
Alex Grant <grantovich@users.noreply.github.com>
2014-07-22 16:33:53 -0400

    Add data for articles demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#show'
+  get 'articles', to: 'articles#index'
   get 'profile', to: 'profiles#show'
   get 'reference', to: 'profiles#reference'
-  root to: 'profiles#show'
 end
