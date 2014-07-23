39a39810b38ba9f4b2f74f3a832b4690ce1318ee
Jodi Detch <jodidetch@gmail.com>
2014-07-23 15:02:58 -0400

    In-class demonstration of how to use Handlebars template

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+  $.ajax({
+    dataType: 'json',
+    url: Routes.articles_path()
+  })
+  .done(function(data){
+    // need to do something with 'data'
+    $('#container').html(HandlebarsTemplates.articles(data));
+  });
 });
